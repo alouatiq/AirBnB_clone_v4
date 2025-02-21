d776864c39273a6fe0b1b8e1d903347d17b14c5b
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-21 11:06:44 +0100

    Create 0-hbnb.py

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from uuid import uuid4
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(exception):
+    """Closes storage"""
+    storage.close()
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """Render the 0-hbnb.html template"""
+    cache_id = str(uuid4())  # Generate a unique cache ID
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    places = storage.all("Place")
+    return render_template('0-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)

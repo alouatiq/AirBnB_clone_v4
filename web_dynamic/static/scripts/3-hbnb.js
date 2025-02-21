1447756d5682f2574c199a9dc6f7e09ad70590c5
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-21 11:23:56 +0100

    Create 3-hbnb.js

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    const selectedAmenities = {};
+
+    // API status check
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+
+    // Handle amenities selection
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        $('#selected_amenities').text(Object.values(selectedAmenities).join(', '));
+    });
+
+    // Fetch places from API
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function (data) {
+            for (const place of data) {
+                const placeHTML = `
+                    <article>
+                        <h2>${place.name}</h2>
+                        <div class="price_by_night">$${place.price_by_night}</div>
+                        <div class="information">
+                            <div class="max_guest">${place.max_guest} Guests</div>
+                            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                        </div>
+                        <div class="description">
+                            ${place.description}
+                        </div>
+                    </article>
+                `;
+                $('.places').append(placeHTML);
+            }
+        }
+    });
+});
